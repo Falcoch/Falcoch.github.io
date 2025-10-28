94c08cd1240328acc9e5f9d37c537e9dbf594917
Théau <theau.baton@etu.univ-amu.fr>
2025-10-28 14:30:20 +0100

    Add more responsive

--- source/public/separator.js
+++ source/public/separator.js
@@ -3,9 +3,3 @@ let ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "#dedede";
 ctx.fillRect(0, 0, 512, 10);
-
-canvas = document.getElementById("separator-projects");
-ctx = canvas.getContext("2d");
-
-ctx.fillStyle = "#dedede";
-ctx.fillRect(0, 0, 512, 10);
